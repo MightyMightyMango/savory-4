c4a0ff71c339e6872e2ea876568a1eaa5571ab0b
Ellen Granoff <granoff.ellen@gmail.com>
2020-12-06 18:59:27 -0500

    Refactor: change functional single recipe to class component

--- client/components/SingleRecipe.js
+++ client/components/SingleRecipe.js
@@ -7,100 +7,113 @@ import {
   deleteRecipeThunk,
   deleteDraftThunk
 } from '../store/recipes'
+import RecipeForm from './RecipeForm'
 
-export const SingleRecipe = props => {
-  const {recipe, getRecipe, deleteDraft, deleteRecipe} = props
-  const recipeId = props.match.params.recipeId
+export class SingleRecipe extends React.Component {
+  constructor(props) {
+    super(props)
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      canEdit: false
+    }
+  }
 
-  useEffect(() => {
-    getRecipe(recipeId)
-  }, [])
+  componentDidMount() {
+    const recipeId = this.props.match.params.recipeId
+    this.props.getRecipe(recipeId)
+  }
 
-  const handleDeleteDraft = event => {
+  handleDeleteDraft = event => {
     event.preventDefault()
-    deleteDraft(recipeId)
+    this.props.deleteDraft(recipeId)
     history.push('/drafts')
   }
 
-  const handleDeleteRecipe = event => {
+  handleDeleteRecipe = event => {
     event.preventDefault()
-    deleteRecipe(recipeId)
+    this.props.deleteRecipe(recipeId)
     history.push('/myrecipes')
   }
 
-  return (
-    <>
-      <Container>
-        <Title>{recipe.name}</Title>
-        {recipe.isDraft ? <Subtitle>Draft</Subtitle> : ''}
-        <Subtitle />
-        <RecipeContainer>
-          <Image src={recipe.imageUrl} />
-          <Details>
-            <div>{recipe.description}</div>
-            <div>Source: {recipe.publisher}</div>
-            <div>Link: {recipe.url}</div>
-            <div>Prep Time: {recipe.prepTime}</div>
-            <div>Cook Time: {recipe.cookTime}</div>
-            <div>Yield: {recipe.yield}</div>
-            <div>Categories: {recipe.categories}</div>
-          </Details>
+  render() {
+    let recipe = this.props.recipe || {}
+    return (
+      <>
+        <Container>
+          <Title>{recipe.name}</Title>
+          {recipe.isDraft ? <Subtitle>Draft</Subtitle> : ''}
+          <Subtitle />
+          <RecipeContainer>
+            <Image src={recipe.imageUrl} />
+            <Details>
+              <div>{recipe.description}</div>
+              <div>Source: {recipe.publisher}</div>
+              <div>Link: {recipe.url}</div>
+              <div>Prep Time: {recipe.prepTime}</div>
+              <div>Cook Time: {recipe.cookTime}</div>
+              <div>Yield: {recipe.yield}</div>
+              <div>Categories: {recipe.categories}</div>
+            </Details>
 
-          <Ingredients>
-            <Subtitle>Ingredients</Subtitle>
-            {recipe.ingredients ? (
-              <ul>
-                {recipe.ingredients.map(ingredient => (
-                  <ListItem key={recipe.ingredients.indexOf(ingredient)}>
-                    {ingredient}
-                  </ListItem>
-                ))}
-              </ul>
-            ) : (
-              ''
-            )}
-          </Ingredients>
-          <Instructions>
-            <Subtitle>Instructions</Subtitle>
-            {recipe.instructions ? (
-              <ul>
-                {recipe.instructions.map(instruction => (
-                  <ListItem key={recipe.instructions.indexOf(instruction)}>
-                    Step 1: {instruction}
-                  </ListItem>
-                ))}
-              </ul>
-            ) : (
-              ''
-            )}
-          </Instructions>
-        </RecipeContainer>
-        {recipe.isDraft ? (
-          <button
-            type="submit"
-            onClick={() => {
-              if (window.confirm('Are you sure you wish to delete this draft?'))
-                handleDeleteDraft(event)
-            }}
-          >
-            Delete Draft
-          </button>
-        ) : (
-          <button
-            type="submit"
-            onClick={() => {
-              if (
-                window.confirm('Are you sure you wish to delete this recipe?')
-              )
-                handleDeleteRecipe(event)
-            }}
-          >
-            Delete Recipe
-          </button>
-        )}
-      </Container>
-    </>
-  )
+            <Ingredients>
+              <Subtitle>Ingredients</Subtitle>
+              {recipe.ingredients ? (
+                <ul>
+                  {recipe.ingredients.map(ingredient => (
+                    <ListItem key={recipe.ingredients.indexOf(ingredient)}>
+                      {ingredient}
+                    </ListItem>
+                  ))}
+                </ul>
+              ) : (
+                ''
+              )}
+            </Ingredients>
+            <Instructions>
+              <Subtitle>Instructions</Subtitle>
+              {recipe.instructions ? (
+                <ul>
+                  {recipe.instructions.map(instruction => (
+                    <ListItem key={recipe.instructions.indexOf(instruction)}>
+                      Step 1: {instruction}
+                    </ListItem>
+                  ))}
+                </ul>
+              ) : (
+                ''
+              )}
+            </Instructions>
+          </RecipeContainer>
+          {recipe.isDraft ? (
+            <button
+              type="submit"
+              onClick={() => {
+                if (
+                  window.confirm('Are you sure you wish to delete this draft?')
+                )
+                  this.handleDeleteDraft(event)
+              }}
+            >
+              Delete Draft
+            </button>
+          ) : (
+            <button
+              type="submit"
+              onClick={() => {
+                if (
+                  window.confirm('Are you sure you wish to delete this recipe?')
+                )
+                  this.handleDeleteRecipe(event)
+              }}
+            >
+              Delete Recipe
+            </button>
+          )}
+        </Container>
+      </>
+    )
+  }
 }
 
 const mapState = state => ({
