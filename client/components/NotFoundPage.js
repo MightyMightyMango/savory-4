19e4745794dfe41e2bb1d2c9c0a91ef3fb889bbb
Micheline Wu <micheline.wu@gmail.com>
2020-12-14 01:25:11 -0500

    Fix: Background loading page fixed.

--- client/components/NotFoundPage.js
+++ client/components/NotFoundPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Container from '../theme/Container'
 
+import Loader from './Loader'
+
 const NotFound = props => (
   <>
     <Container>
-      <h2>Sorry, you must have gotten lost! Let's get you home. </h2>
-      <h2>Starting your trek in...</h2>
+      <Loader />
     </Container>
   </>
 )
