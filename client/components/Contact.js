15f6f529a1c89e9a10aa032c36e1dccfa7e2f28a
Micheline Wu <micheline.wu@gmail.com>
2020-12-18 12:29:31 -0500

    Feature: Contact Page.

---
+++ client/components/Contact.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import styled from 'styled-components'
+import Container from '../theme/Container'
+
+const Contact = props => {
+  return (
+    <Container primary>
+      <h2>
+        Want to get in touch? Contact us at{' '}
+        <ContactLink>
+          <a href="mailto:savoryapp@gmail.com">savoryapp@gmail.com</a>!
+        </ContactLink>
+      </h2>
+      <h2>About Us:</h2>
+      <Profiles>
+        <Profile>
+          <img className="profile-pics" src="/images/profile/ellen-crop.png" />
+          <h3>Ellen Granoff</h3>
+          <p>
+            <a href="https://www.linkedin.com/in/ellengranoff/" target="_blank">
+              LinkedIn
+            </a>
+          </p>
+        </Profile>
+        <Profile>
+          <img className="profile-pics" src="/images/profile/lea-crop.png" />
+          <h3>Lea Overend</h3>
+          <p>
+            <a href="https://www.linkedin.com/in/lea-overend/" target="_blank">
+              LinkedIn
+            </a>
+          </p>
+        </Profile>
+        <Profile>
+          <img
+            className="profile-pics"
+            src="/images/profile/micheline-crop.png"
+          />
+          <h3>Lea Overend</h3>
+          <p>
+            <a href="https://www.linkedin.com/in/micheline-wu/" target="_blank">
+              LinkedIn
+            </a>
+          </p>
+        </Profile>
+      </Profiles>
+    </Container>
+  )
+}
+
+export default Contact
+
+const ContactLink = styled.div`
+  a {
+    color: black;
+    text-decoration: underline;
+    text-underline-position: under;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+`
+
+const Profiles = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-contents: center;
+  padding: 20px;
+  flex-wrap: wrap;
+
+  .profile-pics {
+    border-radius: 50%;
+  }
+
+  a {
+    color: black;
+    text-decoration: underline;
+    text-underline-position: under;
+  }
+
+  a:hover {
+    color: black;
+    text-decoration: none;
+  }
+`
+const Profile = styled.div`
+  padding: 20px;
+`
