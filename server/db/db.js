440b7fdfeef1e24d4f75f613a2aaf6f39db0f1f0
Micheline Wu <micheline.wu@gmail.com>
2021-05-08 23:14:03 -0400

    db permissions adjusted.

--- server/db/db.js
+++ server/db/db.js
@@ -3,30 +3,31 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-//   {
-//     logging: false
-//   }
-// )
-
-const database = process.env.DATABASE_URL || `${databaseName}`
-
-let db = ''
-
-if (process.env.DATABASE_URL) {
-  db = new Sequelize(database)
-} else {
-  db = new Sequelize(database, 'postgres', '', {
-    dialect: 'postgres',
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
     logging: false,
-    ssl: true,
-    dialectOptions: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  })
-}
+    ssl: true
+  }
+)
+
+// const database = process.env.DATABASE_URL || `${databaseName}`
+
+// let db = ''
+
+// if (process.env.DATABASE_URL) {
+//   db = new Sequelize(database)
+// } else {
+//   db = new Sequelize(database, 'postgres', '', {
+//     dialect: 'postgres',
+//     logging: false,
+//     ssl: true,
+//     dialectOptions: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   })
+// }
 
 module.exports = db
 
